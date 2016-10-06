d4e8c641f56577c890415e22121b06478daadbaf
steamcheng <steamcheng@gmail.com>
2016-10-06 07:07:40 -0400

    Update garage_envmonitor.ino

--- garage_envmonitor.ino
+++ garage_envmonitor.ino
@@ -39,9 +39,9 @@ const int doorRelayPin = D5;    // Pin number of door control relay
 
 //Declare variables:
 unsigned long lasttime;
-int doorState = 0;         // variable for reading the Reed Switch status
-int lastDoorState = 0;
-char message_buff[100];
+int doorState = 0;          // variable for reading the Reed Switch status
+int lastDoorState = 0;      // Variable to hold last door position state
+char message_buff[100];     // Message buffer for catching messages subscribed to via callback
 char envmessage_buff[100];
 int ctr = 0;
 
@@ -112,8 +112,7 @@ void setup() {
   mqtt_connect();
   Serial.println(F("Garage Door Monitor Ready!"));
 
-  // Initialize the sensor (it is important to get calibration values stored on the device).
-  
+  // Initialize the BMP sensor (it is important to get calibration values stored on the device).
   Wire.begin(sdaPin, sclPin);  // Since not using the standard sda/scl pins, must initialize them here.
   
   if (pressure.begin()){
@@ -121,86 +120,76 @@ void setup() {
 }
   else
   {
-    // Oops, something went wrong, this is usually a connection problem,
-    // see the comments at the top of this sketch for the proper connections.
-
+    // Oops, something went wrong, this is usually a connection problem.
     Serial.println("BMP180 init fail\n\n");
     while(1); // Pause forever.
   }
 }
 
-void loop()
+void loop()  // Main executable section.
 {
-
 // Reconnect of connection lost.
    if (!client.connected()) {
     mqtt_connect();
-  }
+   }
   
-    
 // Publish temperature and humidity every 15 seconds
-  if (millis() > (lasttime + 15000)) {
+   if (millis() > (lasttime + 15000)) {
     lasttime = millis();
-    pubTempHum();
-  // Insert a call to the getTempPress() function
-getTempPress();
-
-  }
+    pubTempHum();     // Get and publish DHT temp and humidity
+    getTempPress();   // Get and publish BMP180 temp and pressure
+   }
   
-  
-  // read the state of the garage door switch and publish any changes:
+ // read the state of the garage door switch and publish any changes:
   doorState = digitalRead(doorSwitchPin);
-
-  // compare the doorState to its previous state
-  if (doorState != lastDoorState) {
-    // if the state has changed, increment the counter
+   if (doorState != lastDoorState) {  // Compare the doorState to its previous state
+    // If the state has changed, publish the new state
     if (doorState == HIGH) {
       client.publish("openhab/gardoorstatus","Closed");
       Serial.println(F("Door is closed."));
-    } else {
+    } 
+      else {
       client.publish("openhab/gardoorstatus","Open");
       Serial.println(F("Door is OPEN!")); 
     }
-    // Delay a little bit to avoid bouncing
+    // Delay a little bit to avoid switch bouncing
     delay(50);
-  }
-  // save the current state as the last state,
-  //for next time through the loop
+   }
+  // Save the current state as the last state for the next loop iteration
   lastDoorState = doorState;
- client.loop();
-}
+  client.loop();
+}  // End main loop()
 
-
-void callback(char* topic, byte* payload, unsigned int length) {
+// Callback to receive messages on subscribed topics
+void callback(char* topic, byte* payload, unsigned int length)
+{  
   // MQTT inbound Messaging 
-  int i = 0;
-  for(i=0; i<length; i++) {
+ int i = 0;
+   for(i=0; i<length; i++) {
     message_buff[i] = payload[i];
-  }
-  message_buff[i] = '\0';
-  String msgString = String(message_buff);
-//  Serial.println("Inbound: " + String(topic) +":"+ msgString);
+   }
+ message_buff[i] = '\0';
+ String msgString = String(message_buff);
+//  Serial.println("Inbound: " + String(topic) +":"+ msgString);  // Serial output for troubleshooting.
   
-  //Bounce relay
-  if ( msgString == "Click" ) {
+// Activate open/close relay if a message is received to open/close the door. 
+   if ( msgString == "Click" ) {
      digitalWrite(doorRelayPin,HIGH);
      delay(1000); 
      digitalWrite(doorRelayPin,LOW);
-    }
-  }
+   }
+} // End callback()
   
  
- void pubTempHum() {
- 
+void pubTempHum() {  // Publish temperature and humidity readings from sensor.
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
+  float f = dht.readTemperature(true);    // Read temperature as Fahrenheit (isFahrenheit = true)
   
  if (isnan(f) || isnan(h)) {
     Serial.println("Failed to read from DHT");
-    ctr = ctr+1;}
+    ctr = ctr+1;} // Count failures.  Used to trigger restart below.
 
 // Here is where the DHT sensor gets restarted if the sensor hangs up. 
  if (ctr == 3){
@@ -209,35 +198,32 @@ void callback(char* topic, byte* payload, unsigned int length) {
    digitalWrite(DHTPWR,LOW);
    delay(250);
    digitalWrite(DHTPWR,HIGH);
-dht.begin();
-ctr = 0;
- }
+   dht.begin();
+   ctr = 0;
+   }
  
-   // temp and humidity stuff here
+// Publish temp and humidity here
    String fpubString = String(f);
    fpubString.toCharArray(envmessage_buff, fpubString.length()+1);
    Serial.print(F("\nSending DHT temperature ")); Serial.print(f);
    client.publish("openhab/garageEnvironment/temp", envmessage_buff);
    Serial.print(F("\nSending DHT humidity "));  Serial.println(h);
-  String hpubString = String(h); 
-  hpubString.toCharArray(envmessage_buff, hpubString.length()+1);  
+   String hpubString = String(h); 
+   hpubString.toCharArray(envmessage_buff, hpubString.length()+1);  
    client.publish("openhab/garageEnvironment/humidity", envmessage_buff);
- 
-  } 
+} // End pubTempHum()
 
-// Create this function to read and publish from the BMP180
+// Read and publish temperature and pressure from the BMP180
 void getTempPress(){
   
   char bmpmessage_buff[100];
   char status;
   double T,Tf,P,Pa;
 
-  // You must first get a temperature measurement to perform a pressure reading.
-  
+  // Must first obtain a temperature measurement to perform a pressure reading.
   // Start a temperature measurement:
   // If request is successful, the number of ms to wait is returned.
   // If request is unsuccessful, 0 is returned.
-
   status = pressure.startTemperature();
   if (status != 0)
   {
@@ -245,50 +231,52 @@ void getTempPress(){
     delay(status);
 
     // Retrieve the completed temperature measurement:
-    // Note that the measurement is stored in the variable T.
+    // Measurement is stored in the variable T.
     // Function returns 1 if successful, 0 if failure.
-
     status = pressure.getTemperature(T);
     if (status != 0)
     {
-      // Print out the measurement:
-  //    Serial.print("Temperature: ");
-  //    Serial.print(T,2);
-  //    Serial.print(" deg C, ");
+    // -== Serial output for testing and troubleshooting ==-
+    //    Print out the measurement:
+    //    Serial.print("Temperature: ");
+    //    Serial.print(T,2);
+    //    Serial.print(" deg C, ");
+      
+    //    Convert temperature to Farenheit
       Tf = ((9.0/5.0)*T+32.0);
-  //   Serial.print(Tf,2);
-  //   Serial.println(" deg F");
-      // Publish as "BMPtemp"
+    // -== Serial output for testing and troubleshooting ==-
+    //    Serial.print(Tf,2);
+    //    Serial.println(" deg F");
+
       
    // Start a pressure measurement:
    // The parameter is the oversampling setting, from 0 to 3 (highest res, longest wait).
    // If request is successful, the number of ms to wait is returned.
    // If request is unsuccessful, 0 is returned.
-
       status = pressure.startPressure(3);
       if (status != 0)
       {
         // Wait for the measurement to complete:
-        delay(status);
+          delay(status);
 
    // Retrieve the completed pressure measurement:
-   // Note that the measurement is stored in the variable P.
+   // Measurement is stored in the variable P.
    // Note also that the function requires the previous temperature measurement (T).
    // (If temperature is stable, you can do one temperature measurement for a number of pressure measurements.)
    // Function returns 1 if successful, 0 if failure.
-
         status = pressure.getPressure(P,T);
         if (status != 0)
         {
-     // Print out the measurement:
-     //     Serial.print("Absolute pressure: ");
-     //   Serial.print(P,2);
-     //   Serial.print(" mb, ");
-          Pa = (P*0.0295333727);
-     //     Serial.print(Pa,2);
-     //     Serial.print(" inHg");
-          // publish as "Altimeter"
+         Pa = (P*0.0295333727);
+    // -== Serial output for testing and troubleshooting ==-
+    //     Serial.print("Absolute pressure: ");
+    //     Serial.print(P,2);
+    //     Serial.print(" mb, ");
+    //     Serial.print(Pa,2);
+    //     Serial.print(" inHg");
+    // publish as "Altimeter"
 
+    // -== Serial output for testing and troubleshooting ==-
         }
         else Serial.println("error retrieving pressure measurement\n");
       }
@@ -298,17 +286,13 @@ void getTempPress(){
   }
   else Serial.println("error starting temperature measurement\n");
   
-//  delay(30000);  // Pause for 5 seconds.  I think this delay is already built into the other sketch.
-
-
-   // temp and pressure stuff here
+   // Publish BMP180 temp and pressure here
    String tfpubString = String(Tf);
    tfpubString.toCharArray(bmpmessage_buff, tfpubString.length()+1);
    Serial.print(F("\nSending BMP temperature ")); Serial.print(Tf); Serial.print(" Deg F");
    client.publish("openhab/garageEnvironment/bmptemp", bmpmessage_buff);
    Serial.print(F("\nSending BMP pressure "));  Serial.print(Pa); Serial.println(" inHg");
-  String papubString = String(Pa); 
-  papubString.toCharArray(bmpmessage_buff, papubString.length()+1);  
+   String papubString = String(Pa); 
+   papubString.toCharArray(bmpmessage_buff, papubString.length()+1);  
    client.publish("openhab/garageEnvironment/bmppress", bmpmessage_buff); 
-
-}
+}  // End getTempPress()
